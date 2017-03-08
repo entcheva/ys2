0a44fb95217741c725c46b2ccef2861efb42083e
entcheva <christina.entcheva@gmail.com>
2017-03-08 18:47:49 -0500

    Add index.html

---
+++ marketplace/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Offering from './Offering';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello from App
+        <Offering />
+      </div>
+    );
+  }
+}
---
+++ marketplace/src/components/Offering.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Offering extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Offering</h1>
+      </div>
+    );
+  }
+}
