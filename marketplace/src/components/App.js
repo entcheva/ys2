2601e0cbf87bc04a729c0b652d031cbcb504eb07
entcheva <christina.entcheva@gmail.com>
2017-03-08 21:37:35 -0500

    Build marketplace offering box

--- marketplace/src/components/App.js
+++ marketplace/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Offering from './Offering';
+import '../stylesheets/index.scss';
 
 export default class App extends Component {
   render() {
--- marketplace/src/components/Offering.js
+++ marketplace/src/components/Offering.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import '../stylesheets/index.scss';
 
 export default class Offering extends Component {
   render() {
     return (
-      <div>
+      <div className="offering">
+        hi
         <h1>Hello from Offering</h1>
       </div>
     );
